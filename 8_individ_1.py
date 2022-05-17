3fe3be09fc46993bd5054f1a96dcf915e7223fd2
AS-00 <bahtiyorovanasiba332@gmail.com>
2022-05-17 23:49:12 +0300

    change

---
+++ 8_individ_1.py
@@ -0,0 +1,33 @@
+school = {
+    "1a": 20,
+    "1b": 15,
+    "2b": 33,
+    "5a": 17,
+    "7b": 15,
+    }
+print(school)
+
+def add():
+    
+    className = input("Ввидите номер интересующего вас класса ")
+    studentsCount = input("Будьте добры и пожалуйста ввидите количество учеников")
+    school[className] = studentsCount
+    print("Готова!")
+
+def change():
+    add()
+
+def delete():
+    cName = input("class name: ")
+    del school[cName]
+
+while True:
+    command = input("Внимателно читаю, что вы хотите? ")
+    if(command == "add"):
+        add()
+    elif(command == "list"):
+        print(school)
+    elif(command == "change"):
+        change()
+    elif(command == "delete"):
+        delete()
