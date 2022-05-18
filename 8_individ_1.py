31464a2fc31b8967d7f6cbd18a259611b7c43bc7
AS-00 <bahtiyorovanasiba332@gmail.com>
2022-05-18 13:50:56 +0300

    raight

--- 8_individ_1.py
+++ 8_individ_1.py
@@ -1,33 +1,39 @@
-school = {
-    "1a": 20,
-    "1b": 15,
-    "2b": 33,
-    "5a": 17,
-    "7b": 15,
-    }
-print(school)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+from datetime import date
 
-def add():
-    
-    className = input("Ввидите номер интересующего вас класса ")
-    studentsCount = input("Будьте добры и пожалуйста ввидите количество учеников")
-    school[className] = studentsCount
-    print("Готова!")
+if __name__ == '__main__':
+    school = {
+        "1a": 20,
+        "1b": 15,
+        "2b": 33,
+        "5a": 17,
+        "7b": 15,
+        }
+    print(school)
 
-def change():
-    add()
+    def add():
+        
+        className = input("Ввидите номер интересующего вас класса ")
+        studentsCount = input("Будьте добры и пожалуйста ввидите количество учеников")
+        school[className] = studentsCount
+        print("Готова!")
 
-def delete():
-    cName = input("class name: ")
-    del school[cName]
-
-while True:
-    command = input("Внимателно читаю, что вы хотите? ")
-    if(command == "add"):
+    def change():
         add()
-    elif(command == "list"):
-        print(school)
-    elif(command == "change"):
-        change()
-    elif(command == "delete"):
-        delete()
+
+    def delete():
+        cName = input("class name: ")
+        del school[cName]
+
+    while True:
+        command = input("Внимателно читаю, что вы хотите? ")
+        if(command == "add"):
+            add()
+        elif(command == "list"):
+            print(school)
+        elif(command == "change"):
+            change()
+        elif(command == "delete"):
+            delete()
